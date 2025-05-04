b2c31d95e67db25b31b2f0a12a78be444fc663dc
Toufiquer <toufiquerabc@gmail.com>
2025-05-05 02:45:39 +0600

    done fix bug

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
 import SignInPage from './SignInUI';
-import AuthChecker from '../components/AuthChecker';
 
 const Page: React.FC = () => {
   return (
     <div className="w-full">
-      <AuthChecker />
       <SignInPage />
     </div>
   );
