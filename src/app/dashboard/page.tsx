6bd37edd6dbdd299c9ddfd02e566be40bd4d79e3
Toufiquer <toufiquerabc@gmail.com>
2025-05-05 02:24:03 +0600

    done @ add dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -5,12 +5,13 @@
 | @copyright: tecmart-template, May, 2025
 |-----------------------------------------
 */
+
+import SiteNavLayoutClick from './site-nav-layout-click';
+
 const Page = () => {
   return (
-    <main className="bg-slate-800 text-white flex items-center justify-center w-full min-h-screen flex-col p-12">
-      <p>Dashboard</p>
-      <div className="border-1 m-12 w-full min-h-screen flex items-center justify-center">Box 1</div>
-      <div className="border-1 m-12 w-full min-h-screen flex items-center justify-center">Box 2</div>
+    <main className="bg-slate-700 text-white">
+      <SiteNavLayoutClick />
     </main>
   );
 };
