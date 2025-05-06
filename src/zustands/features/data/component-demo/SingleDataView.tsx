068f0ab2a1312609bd4968973ac1fb905e6e0808
Toufiquer <toufiquerabc@gmail.com>
2025-05-07 03:00:47 +0600

    build@v7 - remove rtk-template

--- src/zustands/features/data/component-demo/SingleDataView.tsx
+++ src/zustands/features/data/component-demo/SingleDataView.tsx
@@ -5,56 +5,35 @@
 | @copyright: shreshtho-v3, April, 2025
 |-----------------------------------------
 */
-import { IData } from '@/zustands/features/data/useData'
+import { IData } from '@/zustands/features/data/useData';
 
-import {
-  useUpdateDataMutation,
-  useDeleteDataMutation,
-} from '@/zustands/features/data/useData'
-import LoadingComponent from './components-others/LoadingComponent'
-import ErrorComponent from './components-others/ErrorComponent'
-import { useState } from 'react'
+import { useUpdateDataMutation, useDeleteDataMutation } from '@/zustands/features/data/useData';
+import LoadingComponent from './components-others/LoadingComponent';
+import ErrorComponent from './components-others/ErrorComponent';
+import { useState } from 'react';
 
 const SingleDataView = ({ data }: { data: IData }) => {
-  const [customText, setCustomText] = useState('')
-  const [
-    updateData,
-    {
-      isLoading: isLoadingUpdate,
-      isError: isErrorUpdate,
-      isSuccess: isSuccessUpdate,
-      error: errorUpdate,
-    },
-  ] = useUpdateDataMutation()
-  const [
-    deleteData,
-    {
-      isLoading: isLoadingDelete,
-      isError: isErrorDelete,
-      isSuccess: isSuccessDelete,
-      error: errorDelete,
-    },
-  ] = useDeleteDataMutation()
-  console.log('isSuccessUpdate : ', isSuccessUpdate)
-  console.log('isSuccessDelete: ', isSuccessDelete)
+  const [customText, setCustomText] = useState('');
+  const [updateData, { isLoading: isLoadingUpdate, isError: isErrorUpdate, isSuccess: isSuccessUpdate, error: errorUpdate }] = useUpdateDataMutation();
+  const [deleteData, { isLoading: isLoadingDelete, isError: isErrorDelete, isSuccess: isSuccessDelete, error: errorDelete }] = useDeleteDataMutation();
   const handleUpdate = () => {
-    updateData(data.id, { title: data.title + 1 })
-  }
+    updateData(data.id, { title: data.title + 1 });
+  };
   const handleDelete = () => {
-    deleteData(data.id)
-  }
-  let renderData = <LoadingComponent />
+    deleteData(data.id);
+  };
+  let renderData = <LoadingComponent />;
   if (!isLoadingUpdate && isErrorUpdate) {
-    renderData = <ErrorComponent error={errorUpdate || 'Update error'} />
+    renderData = <ErrorComponent error={errorUpdate || 'Update error'} />;
   }
   if (!isLoadingUpdate && isSuccessUpdate) {
-    setCustomText('Update Successful')
+    setCustomText('Update Successful');
   }
   if (!isLoadingDelete && isSuccessDelete) {
-    setCustomText('Delete Successful')
+    setCustomText('Delete Successful');
   }
   if (!isLoadingDelete && isErrorDelete) {
-    renderData = <ErrorComponent error={errorDelete || ''} />
+    renderData = <ErrorComponent error={errorDelete || ''} />;
   }
   if (!isLoadingUpdate) {
     renderData = (
@@ -65,22 +44,16 @@ const SingleDataView = ({ data }: { data: IData }) => {
         </div>
         <div className="w-full flex items-center justify-end gap-4">
           <p className="px-4 text-sm text-slate-500">{customText}</p>
-          <button
-            className="border-1 rounded-lg border-green-600 text-green-600 px-4 cursor-pointer"
-            onClick={handleUpdate}
-          >
+          <button className="border-1 rounded-lg border-green-600 text-green-600 px-4 cursor-pointer" onClick={handleUpdate}>
             Update
           </button>{' '}
-          <button
-            className="border-1 rounded-lg border-rose-600 text-rose-600 px-4 cursor-pointer"
-            onClick={handleDelete}
-          >
+          <button className="border-1 rounded-lg border-rose-600 text-rose-600 px-4 cursor-pointer" onClick={handleDelete}>
             Delete
           </button>
         </div>
       </main>
-    )
+    );
   }
-  return renderData
-}
-export default SingleDataView
+  return renderData;
+};
+export default SingleDataView;
