3e315a0b293f7c3b5f1a68a0db11be2280d609d1
Toufiquer <toufiquerabc@gmail.com>
2025-05-17 23:21:53 +0600

    done @ connect to db

--- src/lib/mongoose.ts
+++ src/lib/mongoose.ts
@@ -1,8 +1,6 @@
 import mongoose from 'mongoose';
 
-const MONGODB_URI =
-  process.env.MONGODB_URI ||
-  `mongodb+srv://${process.env.dbusername}:${process.env.dbpassword}@templatenextpwa.nyfzuol.mongodb.net/?retryWrites=true&w=majority&appName=templateNextPWA`;
+const MONGODB_URI = process.env.mongooseURI;
 
 if (!MONGODB_URI) {
   throw new Error('Please define the MONGODB_URI environment variable inside .env.local');
@@ -14,7 +12,7 @@ export default async function dbConnect() {
   if (cached.conn) return cached.conn;
 
   if (!cached.promise) {
-    cached.promise = mongoose.connect(MONGODB_URI, { bufferCommands: false }).then(mongoose => {
+    cached.promise = mongoose.connect(MONGODB_URI as string, { bufferCommands: false }).then(mongoose => {
       cached.conn = mongoose.connection;
       return cached.conn;
     });
